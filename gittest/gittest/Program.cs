fca994387734514cd4d4e4def659fb3accf0ca28
Aiion <alex.soderberg97@gmail.com>
2016-08-30 12:36:24 +0200

    tog bort efterfrågan avförnamn då variabeln inte används

--- gittest/gittest/Program.cs
+++ gittest/gittest/Program.cs
@@ -10,15 +10,13 @@ namespace gittest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Skriv in ditt namn:");
-
-            string name1 = Console.ReadLine();
+           
 
             Console.WriteLine("Skriv in ditt efternamn:");
 
             string name2 = Console.ReadLine();
 
-            System.Console.WriteLine("hej " +name2);
+            System.Console.WriteLine("hej " +name2 + "!");
         }
     }
 }
