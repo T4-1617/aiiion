1ab33364f5b9cd887c94c5a910e8562c1ea72712
Aiion <alex.soderberg97@gmail.com>
2016-08-30 12:29:22 +0200

    alpha

--- gittest/gittest/Program.cs
+++ gittest/gittest/Program.cs
@@ -10,7 +10,15 @@ namespace gittest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("HI THERE");
+            Console.WriteLine("Skriv in ditt namn:");
+
+            string name1 = Console.ReadLine();
+
+            Console.WriteLine("Skriv in ditt efternamn:");
+
+            string name2 = Console.ReadLine();
+
+            System.Console.WriteLine("hej " + name1 + " " +name2);
         }
     }
 }
