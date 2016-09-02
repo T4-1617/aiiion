64a2f1b8e68d7187d6a919f6ece0bbfe39192572
Aiion <alex.soderberg97@gmail.com>
2016-09-02 15:30:28 +0200

    KLAR! Nu skrivs ej planen ut så man inte kan fuska, programmet kontrollerar att man inte angett ett värde som är för stort eller för litet när man gissar och kommentarer är mer specifika

--- boatsandwars/boatsandwars/Program.cs
+++ boatsandwars/boatsandwars/Program.cs
@@ -10,71 +10,67 @@ namespace boatsandwars
     {
         static void Main(string[] args)
         {
-            bool[,] BattleField = new bool[7, 7];
+            bool[,] BattleField = new bool[7, 5];
 
-            for (int y = 0; y < 7; y++)
+            for (int y = 0; y < 5; y++)
             {
                 for (int x = 0; x < 7; x++)
                 {
                     BattleField[x, y] = false;
                 }
             }
-            //skapar spelplanen
+            //skapar spelplanen (7x5 rutor)
 
             Random roll = new Random();
             int ships = 4;
             while (ships > 0)
             {
                 int x = roll.Next(0, 7);
-                int y = roll.Next(0, 7);
+                int y = roll.Next(0, 5);
                 BattleField[x, y] = true;
                 ships--;
 
-            }
-
-            //sätter ut skepp slumpmässigt på spelplanen
-            for (int y = 0; y < 7; y++)
-            {
-                for (int x = 0; x < 7; x++)
-                {
-                    string place;
-                    if (BattleField[x, y] == true)
-                    {
-                        place = " X ";
-                        
-                    }
-                    else
-                    {
-                        
-                        place = " O ";
-                        
-                    }
-                    Console.Write(place);
-                }
-                Console.WriteLine();
-            }
-            //skriver ut spelplanen i consolen
+            }//sätter ut 4 skepp slumpmässigt på planen
 
             Console.WriteLine("Skjut mot skeppen genom att ange kordinater till kanonen!");
 
             int shotcount = 0;
             int shipcount = 4;
-            while (shipcount > 0)
+            int strikeX = -1;
+            int strikeY = -1;
+            while (shipcount > 0)//låter en gissa tills man sänkt alla skepp
             {
                 Console.Write("X kordinat:");
-                int strikeX = int.Parse(Console.ReadLine())-1;
-                Console.Write("y kordinat:");
-                int strikeY = int.Parse(Console.ReadLine())-1;
-
-                if (BattleField[strikeX, strikeY] == true)
+                strikeX = int.Parse(Console.ReadLine()) - 1;
+                while (strikeX > 6 || strikeX < 0)//kontrollerar att man angett en tillåten int mellan 1-7
                 {
+                    if (strikeX > 6 || strikeY < 0)//om man inte angett tillåten int får man skriva in ett tal igen
+                    {
+                        Console.WriteLine("ej ett giltigt tal värdet måste vara mellan 1-7, prova igen:");
+                        strikeX = int.Parse(Console.ReadLine()) - 1;
+                    }
+
+                }
+                Console.Write("Y kordinat:");
+                strikeY = int.Parse(Console.ReadLine()) - 1;
+                while (strikeY > 4 || strikeY < 0)//kontrollerar att man angett en tillåten int mellan 1-5
+                {
+                    if (strikeY > 4 || strikeY < 0)//om man inte angett tillåten int får man skriva in ett tal igen
+                    {
+                        Console.WriteLine("ej ett giltigt tal värdet måste vara mellan 1-5, prova igen:");
+                        strikeY = int.Parse(Console.ReadLine()) - 1;
+                    }
+                }
+
+                if (BattleField[strikeX, strikeY] == true) //om man gissat rätt tas skeppet bort ur spelet och den räknar försöket
+                {                                       
                     Console.WriteLine("Hit! Ship down");
                     BattleField[strikeX, strikeY] = false;
                     shotcount++;
                     shipcount--;
                     Console.WriteLine("Ships left: " + shipcount);
                 }
-                else
+                else //gissar man fel så får man föröska igen, försöket räknas även
                 {
                     Console.WriteLine("Miss, try again!");
                     shotcount++;
