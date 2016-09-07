b6d1b388e94aaea4a25e06319b2862336b15e92c
Aiion <alex.soderberg97@gmail.com>
2016-09-07 09:29:18 +0200

    initial commit

---
+++ 21/21/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool[,] deckCards = new bool[13, 4];
+            for (int y = 0; y < 8; y++)
+            {
+                for (int x = 0; x < 13; x++)
+                {
+                    deckCards[x, y] = false;
+                }
+            }
+
+        }
+    }
+}
