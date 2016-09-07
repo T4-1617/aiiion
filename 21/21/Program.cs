f6c0879bb42522da0087a6ce20caaf5ae6386159
Aiion <alex.soderberg97@gmail.com>
2016-09-07 09:55:55 +0200

    started with switch cases

--- 21/21/Program.cs
+++ 21/21/Program.cs
@@ -18,7 +18,45 @@ namespace _21
                     deckCards[x, y] = false;
                 }
             }
+            Random roll = new Random();
+            int cardsLeft = 104;
+            int score = 0;
+            while (cardsLeft > 0)
+            {
+                Console.WriteLine("vill du ha ett kort? Y / N");
+                string hitme = Console.ReadLine();
+                switch (hitme)
+                {
+                    case "y":
+                        int x = roll.Next();
+                        int y = roll.Next();
+                        if (deckCards[x, y] != true)
+                        {
+                            Console.Write("Du fick kortet ");
+                            switch (y)
+                            {
+                                case 0:
+                                case 4:
+                                    Console.Write("hjärter ");
+                                    break;
+                                case 1:
+                                case 5:
+                                    Console.Write("spader ");
+                                    break;
+                                case 2:
+                                case 6:
+                                    Console.Write("ruter ");
+                                    break;
+                                case 3:
+                                case 7:
+                                    Console.Write("klöver ");
+                                    break;
+                            }
+                        }
+                        break;
 
+                }
+            }
         }
     }
 }
