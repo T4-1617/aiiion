8d42feb06369d2a4510314090510688d73ba4722
Aiion <alex.soderberg97@gmail.com>
2016-09-08 13:31:59 +0200

    nu funkar nästan dealern

--- 21/21/Program.cs
+++ 21/21/Program.cs
@@ -10,10 +10,12 @@ namespace _21
     {
         static void Main(string[] args)
         {
+            int x = 0;
+            int y = 0;
             bool[,] deckCards = new bool[13, 16];
-            for (int y = 0; y < 16; y++)
+            for (y = 0; y < 16; y++)
             {
-                for (int x = 0; x < 13; x++)
+                for (x = 0; x < 13; x++)
                 {
                     deckCards[x, y] = false;
                 }
@@ -21,109 +23,202 @@ namespace _21
             Random roll = new Random();
             int cardsLeft = 208;
             int score = 0;
+            int opposition = 0;
+            Boolean cont = false;
+
             while (cardsLeft > 0)
             {
-                Console.WriteLine("vill du ha ett kort? J / N");
-                string hitme = Console.ReadLine();
-                switch (hitme)
+                while (cont == false)
                 {
-                    case "j":
-                    case "J":
-                        int done = 1;
-                        while (done == 1) { 
-                        int x = roll.Next(0, 13);
-                        int y = roll.Next(0, 16);
-                        if (deckCards[x, y] != true)
+                    x = roll.Next(0, 13);
+                    y = roll.Next(0, 16);
+                    if (deckCards[x, y] != true)
+                    {
+                        Console.Write("Dealern fick  ");
+                        switch (y)
                         {
-                            Console.Write("Du fick kortet ");
-                            switch (y)
-                            {
-                                case 0:
-                                case 4:
-                                case 8:
-                                case 12:
-                                    Console.Write("hjärter ");
-                                    break;
-                                case 1:
-                                case 5:
-                                case 9:
-                                case 13:
-                                    Console.Write("spader ");
-                                    break;
-                                case 2:
-                                case 6:
-                                case 10:
-                                case 14:
-                                    Console.Write("ruter ");
-                                    break;
-                                case 3:
-                                case 7:
-                                case 11:
-                                case 15:
-                                    Console.Write("klöver ");
-                                    break;
-                                default:
-                                    Console.WriteLine("ERROR");
-                                    break;
-                            }
-                            switch (x)
-                            {
-                                case 0:
-                                    Console.Write("ess");
-                                        x = 10;
-                                        break;
-                                case 10:
-                                    Console.Write("kneckt");
-                                        x = 10;
-                                        break;
-                                case 11:
-                                    Console.Write("dam");
-                                        x = 10;
-                                        break;
-                                case 12:
-                                    Console.Write("kung");
-                                        x = 10;
-                                    break;
-                                default:
-                                    Console.Write(x + 1);
-                                    break;
-                            }
-                            score = score + x + 1;
-                            deckCards[x, y] = true;
-                            cardsLeft--;
-                            Console.WriteLine(" du ligger på " + score);
-                            done = 0; 
-                            }
+                            case 0:
+                            case 4:
+                            case 8:
+                            case 12:
+                                Console.Write("hjärter ");
+                                break;
+                            case 1:
+                            case 5:
+                            case 9:
+                            case 13:
+                                Console.Write("spader ");
+                                break;
+                            case 2:
+                            case 6:
+                            case 10:
+                            case 14:
+                                Console.Write("ruter ");
+                                break;
+                            case 3:
+                            case 7:
+                            case 11:
+                            case 15:
+                                Console.Write("klöver ");
+                                break;
+                            default:
+                                Console.WriteLine("ERROR");
+                                break;
                         }
-                        break;
-                    case "n":
-                    case "N":
-                        Console.WriteLine("Du slutade på " + score);
-                        score = 0;
-                        Console.WriteLine("Tryck på enter för att spela igen");
-                        Console.ReadLine();
-                        break;
+                        switch (x)
+                        {
+                            case 0:
+                                Console.Write("ess");
+                                x = 10;
+                                break;
+                            case 10:
+                                Console.Write("kneckt");
+                                x = 10;
+                                break;
+                            case 11:
+                                Console.Write("dam");
+                                x = 10;
+                                break;
+                            case 12:
+                                Console.Write("kung");
+                                x = 10;
+                                break;
+                            default:
+                                Console.Write(x + 1);
+                                break;
+                        }
+                        opposition = opposition + x + 1;
+                        deckCards[x, y] = true;
+                        cardsLeft--;
+                        Console.WriteLine(" Dealern har " + opposition);
+                        cont = true;
+                    }
+                }
 
-                    default:
-                        Console.WriteLine("Du måste svara med antingen n eller j");
-                        break;
+                        Console.WriteLine("vill du ha ett kort? J / N");
+                        string hitme = Console.ReadLine();
+                        switch (hitme)
+                        {
+                            case "j":
+                            case "J":
+                                int done = 1;
+                                while (done == 1)
+                                {
+                                    x = roll.Next(0, 13);
+                                    y = roll.Next(0, 16);
+                                    if (deckCards[x, y] != true)
+                                    {
+                                        Console.Write("Du fick kortet ");
+                                        switch (y)
+                                        {
+                                            case 0:
+                                            case 4:
+                                            case 8:
+                                            case 12:
+                                                Console.Write("hjärter ");
+                                                break;
+                                            case 1:
+                                            case 5:
+                                            case 9:
+                                            case 13:
+                                                Console.Write("spader ");
+                                                break;
+                                            case 2:
+                                            case 6:
+                                            case 10:
+                                            case 14:
+                                                Console.Write("ruter ");
+                                                break;
+                                            case 3:
+                                            case 7:
+                                            case 11:
+                                            case 15:
+                                                Console.Write("klöver ");
+                                                break;
+                                            default:
+                                                Console.WriteLine("ERROR");
+                                                break;
+                                        }
+                                        switch (x)
+                                        {
+                                            case 0:
+                                                Console.Write("ess");
+                                                x = 9;
+                                                break;
+                                            case 10:
+                                                Console.Write("kneckt");
+                                                x = 9;
+                                                break;
+                                            case 11:
+                                                Console.Write("dam");
+                                                x = 9;
+                                                break;
+                                            case 12:
+                                                Console.Write("kung");
+                                                x = 9;
+                                                break;
+                                            default:
+                                                Console.Write(x + 1);
+                                                break;
+                                        }
+                                        score = score + x + 1;
+                                        deckCards[x, y] = true;
+                                        cardsLeft--;
+                                        Console.WriteLine(" du ligger på " + score);
+                                        done = 0;
+                                        
+                                    }
+
+
+                                }
+                                break;
+                            case "n":
+                            case "N":
+                                Console.WriteLine("Du slutade på " + score);
+                                score = 0;
+                                Console.WriteLine("Tryck på enter för att spela igen");
+                                Console.ReadLine();
+                                break;
+
+                            default:
+                                Console.WriteLine("Du måste svara med antingen n eller j");
+                                break;
+
+                        }
+                        if (score > 21)
+                        {
+                            Console.WriteLine("DU SPRACK! Din total blev {0} Bättre lycka nästa gång!", score);
+                            score = 0;
+                            Console.WriteLine("Tryck på enter för att spela igen");
+                            Console.ReadLine();
+                            cont = false;
+                            opposition = 0;
+
+                        }
+                        else if (score == 21)
+                        {
+                            Console.WriteLine("DU VANN! Grattis!");
+                            score = 0;
+                            Console.WriteLine("Tryck på enter för att spela igen");
+                            Console.ReadLine();
+                            cont = false;
+                            opposition = 0;
+                        }
+                    if (score > opposition && score < 21)
+                    {
+                        cont = false;
+                    }
+                    else
+                    {
+                        cont = true;
+                    }
+
 
                 }
-                if (score > 21) {
-                    Console.WriteLine("DU SPRACK! Din total blev {0} Bättre lycka nästa gång!", score);
-                    score = 0;
-                    Console.WriteLine("Tryck på enter för att spela igen");
-                    Console.ReadLine();
+                    Console.WriteLine("Tvärr finns det inga fler kort, spelet avslutas");
+                
 
-                }
-                else if (score == 21){
-                    Console.WriteLine("DU VANN! Grattis!");
-                    score = 0;
-                    Console.WriteLine("Tryck på enter för att spela igen");
-                    Console.ReadLine();
-                }
             }
-            Console.WriteLine("Tvärr finns det inga fler kort, spelet avslutas");
         }
     }
-}
+
