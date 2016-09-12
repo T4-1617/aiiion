270365be86047dcba11094bc5397ca8003397a3b
Aiion <alex.soderberg97@gmail.com>
2016-09-12 13:37:10 +0200

    inital commit

---
+++ Winformklass/Winformklass/Customer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Winformklass
+{
+    class Customer
+    {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+
+        public string Fullname()
+        {
+            return string.Format("{0} {1}", Firstname, Lastname);
+        }
+    }
+}
---
+++ Winformklass/Winformklass/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Winformklass
+{
+    public partial class Form1 : Form
+    {
+        System.Collections.ArrayList MyCustomers = new System.Collections.ArrayList();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Customer cus = new Customer();
+
+            cus.Firstname = Firstname.Text;
+            cus.Lastname = Lastname.Text;
+
+            MyCustomers.Add(cus);
+
+            MessageBox.Show(string.Format("added the customer {0}", cus.Fullname()));
+        }
+    }
+}
